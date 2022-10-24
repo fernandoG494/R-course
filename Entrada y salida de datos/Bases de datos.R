d9dba212a9ed90d957f8e3c6022ad1dd95c97520
fernandoG494 <42920098+fernandoG494@users.noreply.github.com>
2022-10-24 14:10:17 -0500

    DB

---
+++ Entrada y salida de datos/Bases de datos.R
@@ -0,0 +1,15 @@
+# Bases de datos
+# instalar paquete SQL
+install.packages('RSQLite')
+# referencia a la libreria
+library(RSQLite)
+
+connection <- dbConnect(RSQLite::SQLite(), "basededatos.db")
+dbWriteTable(connection, "coches", mtcars)
+dbReadTable(connection, "coches")
+
+res <- dbSendQuery(connection, 'SELECT * from coches WHERE cyl = 8')
+dbFetch(res)
+
+dbClearResult(res)
+dbD
